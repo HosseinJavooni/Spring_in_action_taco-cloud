a2e5dba1b9425ca4028e5ccccfd2a51be12ba107
Hossein Jabani <h.javooni@gmail.com>
2024-01-03 10:31:30 +0330

    Wed Jan  3 10:31:30 AM +0330 2024

--- src/main/java/tacos/model/Ingredient.java
+++ src/main/java/tacos/model/Ingredient.java
@@ -2,11 +2,15 @@ package tacos.model;
 
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 public class Ingredient {
     private final String id;
     private final String name;
     private final Type type;
+    Objects objects;
+
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
